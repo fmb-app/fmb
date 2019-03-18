c474b9016dad9a28532bcb90ff1eca3830730be8
Sara <saraenge@kth.se>
2019-03-18 18:00:18 +0100

    feat: added schemas for store and products

--- src/server/models/products.mjs
+++ src/server/models/products.mjs
@@ -6,8 +6,15 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 var productSchema = new Schema({
-  name: { type: String, required: true },
   productId: { type: Number, required: true },
+  name1: { type: String, required: true },
+  name2: String,
+  group: String,
+  price: Number,
+  volume: Number,
+  package: String,
+  alcohol: Number,
+  producer: String
 });
 
 export default mongoose.model('Product', productSchema);
--- src/server/models/stores.mjs
+++ src/server/models/stores.mjs
@@ -6,11 +6,20 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 var storeSchema = new Schema({
+  storeId: { type: Number, required: true },
   name: { type: String, required: true },
-  longitude: { type: Number, required: true },
-  latitude: { type: Number, required: true },
-  address: { type: String, required: true }
+  street: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  city: { type: String, required: true },
+  region: { type: String, required: true },
+  rt90x: { type: Number, required: true },
+  rt90y: { type: Number, required: true },
+  products: [productSchema]
 });
 
 export default mongoose.model('Store', storeSchema);
+
+
+
+
 
