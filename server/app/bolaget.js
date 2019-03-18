79b4b84f40e767fb589300cc01c7476c10c78965
Shapour Jahanshahi <shappan@gmail.com>
2019-03-18 11:14:36 +0100

    Changed data collection behavior

--- server/app/bolaget.js
+++ server/app/bolaget.js
@@ -2,16 +2,39 @@
 import systemet from 'systemet';
 import Store from './models/stores';
 import Product from './models/products';
+import cheerio from 'cheerio';
 
-export const products = () => {
-  console.log('Getting All Products:');
-  return systemet.products();
+const API_URL = 'http://www.systembolaget.se/api';
+
+export let stores = [];
+export let products = [];
+export let stocks = [];
+
+export const allProducts = () => {
+  systemet.products()
+    .then(allProducts => {
+      products = allProducts.map(product => product.name + '; ' + product.nameExtra);
+      console.log('Products loaded');
+    });
 };
 
-export const stores = () => {
+export const allStocks = () => {
+  fetch('http://www.systembolaget.se/api/assortment/stock/xml')
+    .then(res => {
+      const $ = cheerio.load(res.body, {
+        normalizeWhitespace: true,
+        xmlMode: true
+      });
+      console.log($);
+      // return $('artikel').map((i, el) => toProduct($, el)).get();
+    });
+};
+
+export const allStores = () => {
   systemet.stores()
     .then(allStores => {
-      return allStores;
+      stores = allStores.filter(store => store.type === 'Butik' && store.county === 'Stockholms län');
+      console.log('Stores loaded');
     });
 };
 
@@ -21,8 +44,11 @@ const myStore = new Store({
   latitude: 456,
   address: 'Birger Jarlsgatan 142'
 });
-
 console.log(myStore.name);
 
-
-
+console.log('Stores loading...');
+allStores();
+console.log('Products loading...');
+allProducts();
+// console.log('Stocks loading...');
+// allStocks();
