479eb3277e1d5f689806b9f23911e0518db293b7
Shapour Jahanshahi <shappan@gmail.com>
2019-03-18 11:12:24 +0100

    Added sample routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,9 +2,26 @@
 
 import express from 'express';
 import path from 'path';
-
+import { stores, products, stocks } from '../bolaget';
 
 const router = express.Router();
+
+router.get('/stores/:long/:lat', (req, res) => {
+  // req.params.long
+  res.send('Finds the closest stores to my coordinates.');
+});
+
+router.get('/stores', (req, res) => {
+  res.json(stores);
+});
+
+router.get('/stocks', (req, res) => {
+  res.json(stocks);
+});
+
+router.get('/products', (req, res) => {
+  res.json(products);
+});
 
 router.get('/', (req, res) => {
   let msg = '👋 Yo world.';
