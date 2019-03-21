b6d8ea0bbb041a0411593ce2d4aa2adecccdb74c
Shapour Jahanshahi <shappan@gmail.com>
2019-03-21 13:27:19 +0100

    fix: removed unused attributes

--- src/server/models/stores.mjs
+++ src/server/models/stores.mjs
@@ -1,21 +1,17 @@
 'use strict';
-import systemet from 'systemet';
-import cheerio from 'cheerio';
 import mongoose from 'mongoose';
-import { productSchema } from './products';
 
 const Schema = mongoose.Schema;
 
 var storeSchema = new Schema({
-  _id: { type: Number, required: true },
+  _id: { type: String, required: true },
   name: { type: String },
   street: { type: String, required: true },
   postalCode: { type: String, required: true },
   city: { type: String, required: true },
   rt90x: { type: Number, required: true },
   rt90y: { type: Number, required: true },
-  openingHours: { type: Object },
-  products: [ productSchema ]
+  openingHours: { type: Object }
 });
 
 export default mongoose.model('Store', storeSchema);
