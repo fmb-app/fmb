a9df9498237437f69132ac2503d6fb8420055367
Shapour Jahanshahi <shappan@gmail.com>
2019-03-21 20:16:32 +0100

    Fixed return

--- src/server/APICalls/googleAPIcall.mjs
+++ src/server/APICalls/googleAPIcall.mjs
@@ -5,8 +5,7 @@ const httpOptions = {
 };
 
 export const googleFetch = (long, lat) => {
-  fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + long + "," + lat + "&name=systembolaget&rankby=distance&key=" + process.env.GOOGLE_API)
+  return fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + long + "," + lat + "&name=systembolaget&rankby=distance&key=" + process.env.GOOGLE_API)
     .then((res) => res.json())
-    .then((res) => console.log(res))
     .catch((error) => console.log(error))
 }
