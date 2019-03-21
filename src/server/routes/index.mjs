1de02d600d5ac25c064b1f8e170b32476a96555e
Shapour Jahanshahi <shappan@gmail.com>
2019-03-21 20:15:42 +0100

    Added stores route

--- src/server/routes/index.mjs
+++ src/server/routes/index.mjs
@@ -1,32 +1,37 @@
 'use strict';
 
 import express from 'express';
-import path from 'path';
-import { stores, products, stocks } from '../bolaget';
-import {googleFetch} from '../APICalls/googleAPIcall'
+import { findStoresWithGivenProductNrs } from '../bolaget/bolaget';
+import { googleFetch } from '../APICalls/googleAPIcall'
 
 const router = express.Router();
 
+
+// Get the 20 closest Systembolaget stores from the given coordinates. Uses Google Places API.
 router.get('/stores/:long/:lat', (req, res) => {
-  // req.params.long
-  res.send('Finds the closest stores to my coordinates.');
+  googleFetch(req.params.long, req.params.lat)
+    .then((data) => {
+      res.json(data);
+    });
 });
 
-router.get('/stores', (req, res) => {
-  res.json(stores);
-});
+// router.get('/stores', (req, res) => {
+//   res.json(stores);
+// });
 
-router.get('/stocks', (req, res) => {
-  res.json(stocks);
-});
+// router.get('/stocks', (req, res) => {
+//   res.json(stocks);
+// });
 
-router.get('/products', (req, res) => {
-  res.json(products);
-});
+// router.get('/products', (req, res) => {
+//   res.json(products);
+// });
 
-router.get('/googleAPI', (req, res) => {
-  res.json(googleFetch(59.3633306,17.871843));
-})
+// router.get('/googleAPI', (req, res) => {
+//   googleFetch(59.3633306,17.871843).then((data) => {
+//     res.json(data);
+//   });
+// })
 
 router.get('/', (req, res) => {
   let msg = '👋 Yo world.';
