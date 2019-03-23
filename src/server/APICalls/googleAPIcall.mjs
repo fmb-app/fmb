f208d4f5ca2e1b6cfc6b4d57795a77919c8f0a58
Shapour Jahanshahi <shappan@gmail.com>
2019-03-23 18:05:29 +0100

    Added offset for querys with more than 20 results

--- src/server/APICalls/googleAPIcall.mjs
+++ src/server/APICalls/googleAPIcall.mjs
@@ -4,7 +4,7 @@ const httpOptions = {
   headers: { "key": process.env.GOOGLE_API}
 };
 
-export const googleFetch = (long, lat) => {
+export const googleFetch = (long, lat, offset = 0) => {
   return fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + long + "," + lat + "&name=systembolaget&rankby=distance&key=" + process.env.GOOGLE_API)
     .then((res) => res.json())
     .catch((error) => console.log(error))
