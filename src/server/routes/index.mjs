6d88b2ace14de4295d775a6f69c5c8e3ab4c3917
Shapour Jahanshahi <shappan@gmail.com>
2019-03-23 18:05:50 +0100

    feat: Created /stores post route

--- src/server/routes/index.mjs
+++ src/server/routes/index.mjs
@@ -1,7 +1,7 @@
 'use strict';
 
 import express from 'express';
-import { findStoresWithGivenProductNrs } from '../APICalls/bolaget';
+import { getStoresWithProducts, getNrOfStores } from '../APICalls/bolaget';
 import { googleFetch } from '../APICalls/googleAPIcall'
 
 const router = express.Router();
@@ -27,11 +27,47 @@ router.get('/stores/:long/:lat', (req, res) => {
 //   res.json(products);
 // });
 
-// router.get('/googleAPI', (req, res) => {
-//   googleFetch(59.3633306,17.871843).then((data) => {
-//     res.json(data);
-//   });
-// })
+// {
+// 	"coords": {
+// 		"long": "59.3633306",
+// 		"lat": "17.874037"
+// 	},
+// 	"productNrs": ["8685501", "141212"]
+// }
+
+// Get the stores that holds all the given productNrs, sorted by distance.
+router.post('/stores', async (req, res) => {
+  const long = req.body.coords.long;
+  const lat = req.body.coords.lat;
+  const productNrs = req.body.productNrs;
+  // Validate post body
+  if (Number(long) === NaN || Number(lat) === NaN || productNrs.some(nr => Number(nr) === NaN)) {
+    res.sendStatus(404);
+  };
+  const storesFromBolaget = await getStoresWithProducts(productNrs);
+  let storesLeft = getNrOfStores();
+  let offset = 0;
+  let closeStores = [];
+
+  while (storesLeft > 0 && closeStores.length < 3) {
+
+    let googleResults = await googleFetch(long, lat, offset);  
+    let storesFromGoogle = await googleResults;
+    let closestStoresWithProducts = storesFromGoogle.results.filter(store => {
+      return storesFromBolaget.some(bolag => {
+        return store.vicinity.toLocaleLowerCase('sv').includes(bolag.street.toLocaleLowerCase('sv'));
+      })
+    });
+    closeStores = [...closeStores, ...closestStoresWithProducts];
+    console.log('Total stores: ', closeStores.length);
+    closeStores.map(store => console.log(store.vicinity))
+    storesLeft -= 20;
+    offset += 20;
+  }
+
+
+  res.json({stores: closeStores});
+})
 
 router.get('/', (req, res) => {
   let msg = '👋 Yo world.';
