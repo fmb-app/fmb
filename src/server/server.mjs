80a233efc28b437348de0ec0dbedfe81bfd20121
Shapour Jahanshahi <shappan@gmail.com>
2019-03-25 22:59:59 +0100

    Added 404 page

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -16,6 +16,29 @@ app.use(express.urlencoded({ extended: true })); // Parse URLs
 app.use('/api', routes);
 app.use('/', express.static('dist'));
 
+// Set a 404 if the route wasn't found.
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  res.status(404);
+  next(err);
+});
+
+// Middleware that handles errors.
+app.use((err, req, res, next) => {
+  res.status(res.statusCode || 500);
+  let data = {
+    message: err.message,
+    status: res.statusCode,
+  };
+  if (app.get('env') === 'development') {
+    data.error = err;
+  }
+  res.send(`<html><body><h1>${res.statusCode}</h1><div>${err.message}</div></body></html>`);
+});
+
+app.use((err, req, res, next) => {
+});
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }); // Connect to db
 
 const PORT = process.env.PORT || 8080; // Port
