99715c5dd997453b845ec7cb88bf854528f9ed55
Sara <saraenge@kth.se>
2019-03-25 14:43:38 +0100

    fixed travel route call to trafiklab API with start and destination coordinates

--- src/server/APICalls/slAPIcall.mjs
+++ src/server/APICalls/slAPIcall.mjs
@@ -1,12 +1,10 @@
 'use strict'
 import fetch from "node-fetch";
-// const httpOptions = {
-//   headers: { "key": process.env.SL_API}
-// };
 
-export const slFetch = () => {			//long, lat, offset = 0) => {
-	return fetch("https://api.sl.se/api2/TravelplannerV3_1/trip.json?key="+ process.env.SL_API +"&lang=se&originExtId=TCE&destExtId=9111&maxChange=3&lines=!19")
-  // return fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + long + "," + lat + "&name=systembolaget&rankby=distance&key=" + process.env.GOOGLE_API)
+export const slFetch = (startLat, startLong, destLat, destLong) => {
+	return fetch("https://api.sl.se/api2/TravelplannerV3_1/trip.json?key="+ process.env.SL_API +
+		"&lang=se&originCoordLat=" + startLat + "&originCoordLong=" + startLong + "&destCoordLat=" + destLat + "&destCoordLong=" + destLong)
     .then((res) => res.json())
     .catch((error) => console.log(error))
 }
+
