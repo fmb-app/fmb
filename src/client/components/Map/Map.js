0369cc18649e916509e33274ecc1b572bbc13414
Daniel <ddah@kth.se>
2019-04-16 18:11:58 +0200

    feat : added marker control to the map

--- src/client/components/Map/Map.js
+++ src/client/components/Map/Map.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import FmbContext from '../../context/FmbContext'
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
 
+
 const Map = () => {
+  const context = useContext(FmbContext);
+  const setMapCoordinates = (e) => {
+    console.log(e)
+    const {lat,lng} = e.target._latlng
+    console.log([lat,lng])
+    context.setCoordinates([lat,lng])
+  }
     return (
       <div>
       <center>
-        <LeafletMap 
-          center={[59.3498, 18.0707]} 
+        <LeafletMap
+          center={[59.3498, 18.0707]}
           zoom={10}
           maxZoom={20}
           attributionControl={true}
@@ -21,7 +30,7 @@ const Map = () => {
           <TileLayer
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           />
-          <Marker position={[59.3498, 18.0707]} draggable={true}>
+          <Marker position={context.location.data} draggable={true} onDragEnd={setMapCoordinates}>
             <Popup>
               Popup for any custom information.
             </Popup>
