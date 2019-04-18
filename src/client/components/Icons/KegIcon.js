75b9a9de49757c0e0268005b9e2c73613752336b
Sara <saraenge@kth.se>
2019-04-19 00:15:52 +0200

    added more icons

---
+++ src/client/components/Icons/KegIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import kegIcon from '../../resources/images/icons8-wooden-beer-keg-filled-100.png';
+
+const KegIcon = ({height, width, color}) => {
+	return (
+		<img
+			src={kegIcon}
+			alt='Keg'
+			heigth={height}
+			width={width}
+		/>
+	);
+}
+
+export default KegIcon;
