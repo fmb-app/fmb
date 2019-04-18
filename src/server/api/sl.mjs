cefae957081cb8bb37e9888780ee7fbc225ca1f1
Shapour Jahanshahi <shappan@gmail.com>
2019-04-18 18:19:34 +0200

    fix: removed console log

--- src/server/api/sl.mjs
+++ src/server/api/sl.mjs
@@ -2,8 +2,6 @@
 import fetch from "node-fetch";
 
 export const slFetch = (startLat, startLong, destLat, destLong) => {
-	console.log("slapi: https://api.sl.se/api2/TravelplannerV3/trip.json?key="+ process.env.SL_API_3 +
-		"&lang=se&originCoordLat=" + startLat + "&originCoordLong=" + startLong + "&destCoordLat=" + destLat + "&destCoordLong=" + destLong);
 	return fetch("https://api.sl.se/api2/TravelplannerV3/trip.json?key="+ process.env.SL_API_3 +
 		"&lang=se&originCoordLat=" + startLat + "&originCoordLong=" + startLong + "&destCoordLat=" + destLat + "&destCoordLong=" + destLong)
     .then((res) => res.json())
