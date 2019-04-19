c90a6459091e3a4c3c8a23eb11345ce9941987cf
Shapour Jahanshahi <shappan@gmail.com>
2019-04-19 18:24:26 +0200

    debug print

--- src/server/api/sl.mjs
+++ src/server/api/sl.mjs
@@ -2,8 +2,10 @@
 import fetch from "node-fetch";
 
 export const slFetch = (startLat, startLong, destLat, destLong) => {
-	return fetch("https://api.sl.se/api2/TravelplannerV3/trip.json?key="+ process.env.SL_API_3 +
-		"&lang=se&originCoordLat=" + startLat + "&originCoordLong=" + startLong + "&destCoordLat=" + destLat + "&destCoordLong=" + destLong)
+  const url = "https://api.sl.se/api2/TravelplannerV3/trip.json?key="+ process.env.SL_API_3 +
+  "&lang=se&originCoordLat=" + startLat + "&originCoordLong=" + startLong + "&destCoordLat=" + destLat + "&destCoordLong=" + destLong;
+  console.log(url);
+	return fetch(url)
     .then((res) => res.json())
     .catch((error) => console.log(error))
 }
