ff18c9e074fb1cc06b9117e35b2eb4fdfa105239
Shapour Jahanshahi <shappan@gmail.com>
2019-04-19 18:33:12 +0200

    fix: fixed SL issue

--- src/server/api/sl.mjs
+++ src/server/api/sl.mjs
@@ -2,10 +2,8 @@
 import fetch from "node-fetch";
 
 export const slFetch = (startLat, startLong, destLat, destLong) => {
-  const url = "https://api.sl.se/api2/TravelplannerV3/trip.json?key="+ process.env.SL_API_3 +
-  "&lang=se&originCoordLat=" + startLat + "&originCoordLong=" + startLong + "&destCoordLat=" + destLat + "&destCoordLong=" + destLong;
-  console.log(url);
-	return fetch(url)
+	return fetch("https://api.sl.se/api2/TravelplannerV3/trip.json?key="+ process.env.SL_API_3 +
+		"&lang=se&originCoordLat=" + startLat + "&originCoordLong=" + startLong + "&destCoordLat=" + destLat + "&destCoordLong=" + destLong)
     .then((res) => res.json())
     .catch((error) => console.log(error))
 }
