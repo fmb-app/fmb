89a39de3042ec103769c122fe17d181f922d6bf0
Shapour Jahanshahi <shappan@gmail.com>
2019-04-23 01:34:28 +0200

    feature: separated label and sublabel

--- src/client/components/ExpandableContainer/ExpandableContainer.js
+++ src/client/components/ExpandableContainer/ExpandableContainer.js
@@ -12,25 +12,30 @@ const style = {
 		borderRadius: themes.standardRadius,
 		backgroundColor: '#262632',
 	},
-	top: {
-		width: '100%',
-		padding: themes.standardSpace,
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		boxSizing: 'border-box',
-		':hover': {
-			cursor: 'pointer',
-			backgroundColor: '#363642'
+	top: (hover, expanded) => { 
+		return {
+			width: '100%',
+			padding: themes.standardSpace,
+			display: 'flex',
+			flexDirection: 'row',
+			justifyContent: 'space-between',
+			alignItems: 'center',
+			boxSizing: 'border-box',
+			':hover': {
+				cursor: 'pointer',
+				backgroundColor: (hover || !expanded) && '#363642'
+			}
 		}
 	}
 };
 
-const Top = ({style, label, expand, isExpanded}) => {
+const Top = ({style, label, subLabel, expand, isExpanded, labelStyle, subLabelStyle}) => {
 	return (
 		<div style={style} onClick={expand}>
-			<h3>{label}</h3>
+			<div>
+				<div style={{...labelStyle}}>{label || subLabel}</div>
+				<div style={{...subLabelStyle}}>{label && subLabel}</div>
+			</div>
 			<ExpandButton rotated={isExpanded} />
 		</div>
 	)
@@ -38,21 +43,24 @@ const Top = ({style, label, expand, isExpanded}) => {
 
 const StyledTop = Radium(Top);
 
-const ExpandableContainer = ({label, children, styleProps}) => {
+const ExpandableContainer = ({label, subLabel, hover, children, labelStyle, subLabelStyle}) => {
 	const [expanded, setExpanded] = useState(false);
 
 	const expandedStyle = {
 		transition: 'height 0.5s ease-in-out',
-		height: expanded ? '10rem' : '0rem',
+		height: expanded ? '12rem' : '0rem',
 		overflowY: 'auto'
 	}
 
 	return (
 		<div style={style.container}>
 			<StyledTop
-				style={{...style.top, ...styleProps}}
+				style={{...style.top(hover, expanded)}}
 				expand={() => { setExpanded(!expanded)}}
 				label={label}
+				labelStyle={labelStyle}
+				subLabelStyle={subLabelStyle}
+				subLabel={subLabel}
 				isExpanded={expanded}
 			/>
 			<div style={expandedStyle}>
