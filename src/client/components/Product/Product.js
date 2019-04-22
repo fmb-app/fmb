8bdceb8729c37d3b3e5161f77f79b69c0f490bbd
Shapour Jahanshahi <shappan@gmail.com>
2019-04-22 23:01:49 +0200

    feature: added volume styling

--- src/client/components/Product/Product.js
+++ src/client/components/Product/Product.js
@@ -1,34 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { themes } from '../../themes/Themes';
+import Radium from 'radium';
 
-const productStyle = isSelected => {
-	return {
-		padding: themes.standardSpace,
-		borderRadius: themes.standardRadius,
-		backgroundColor: isSelected ? '#744253' : '#262632',
-		marginTop: themes.smallSpace,
-		height: '2rem',
-		overflow: 'hidden',
-	  whiteSpace: 'nowrap',
+const style = {
+	productStyle: (isSelected) => {
+		return {
+			padding: themes.standardSpace,
+			borderRadius: themes.standardRadius,
+			backgroundColor: isSelected ? '#744253' : '#262632',
+			marginTop: themes.smallSpace,
+			height: '2rem',
+			overflow: 'hidden',
+			whiteSpace: 'nowrap',
+			textOverflow: 'ellipsis',
+			boxSizing: 'border-box',
+			display: 'flex',
+			justifyContent: 'space-between',
+			alignItems: 'center',
+			':hover': {
+				backgroundColor: isSelected ? '#845263' : '#363642',
+				cursor: 'pointer'
+			}
+		}
+	},
+	label: {
+		width: '80%',
 		textOverflow: 'ellipsis',
-		boxSizing: 'border-box',
+		overflow: 'hidden',
+		textAlign: 'left'
+	},
+	volume : {
 		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
+		fontSize: '0.6rem',
+		alignItems: 'flex-end'
 	}
 }
 
 const Product = ({label, altLabel, onClick, isSelected, icon, volume}) => {
-	const productLabel = () => {
-		return (altLabel !== null) ? label + ' - ' + altLabel : label;
-	}
+	const productLabel = () => altLabel !== null ? label + ' - ' + altLabel : label;
 
 	return (
-		<div style={productStyle(isSelected)} onClick={onClick}>
-			<p style={{width: '80%', textOverflow: 'ellipsis', overflow: 'hidden', textAlign: 'left'}}>{productLabel()} </p>
-			{volume}ml {icon}
+		<div style={style.productStyle(isSelected)} onClick={onClick}>
+			<div style={style.label}>{productLabel()} </div>
+			<span style={style.volume}>
+				{volume / 10}cl{icon}
+			</span>
 		</div>
 	);
 }
 
-export default Product;
+export default Radium(Product);
