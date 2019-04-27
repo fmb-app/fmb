dbe37956347fb42fcf8633a0039cd0afd9d46f81
Shapour Jahanshahi <shapour@kth.se>
2019-04-28 00:01:59 +0200

    feat: Added close button and fixed maxWidth issue

--- src/client/components/Map/Map.js
+++ src/client/components/Map/Map.js
@@ -1,15 +1,22 @@
 import React, {useContext} from 'react'
 import FmbContext from '../../context/FmbContext'
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import CloseButton from '../Buttons/CloseButton';
 
 const style = {
   map: {
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    maxWidth: '100vw'
+  },
+  closeButton: {
+    position: 'relative',
+    top: '2rem',
+    zIndex: '999'
   }
 }
 
-const Map = () => {
+const Map = ({toggleMap}) => {
   const context = useContext(FmbContext);
 
   const setMapCoordinates = (e) => {
@@ -17,28 +24,36 @@ const Map = () => {
     context.setCoordinates({lat: lat, long: lng})
   }
     return (
-      <div style={style.map}>
-        <LeafletMap
-          center={[59.3498, 18.0707]}
-          zoom={10}
-          maxZoom={20}
-          attributionControl={true}
-          zoomControl={true}
-          doubleClickZoom={true}
-          scrollWheelZoom={true}
-          dragging={true}
-          animate={true}
-          easeLinearity={0.35}
-        >
-          <TileLayer
-            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      <div style={{display: 'flex', flexFlow: 'column nowrap'}}>
+        <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <CloseButton
+            style={style.closeButton}
+            onClick={toggleMap}
           />
-          <Marker position={[context.location.lat, context.location.long]} draggable={true} onDragEnd={setMapCoordinates}>
-            <Popup>
-              Popup for any custom information.
-            </Popup>
-          </Marker>
-        </LeafletMap>
+        </div>
+        <div style={style.map}>
+          <LeafletMap
+            center={[59.3498, 18.0707]}
+            zoom={10}
+            maxZoom={20}
+            attributionControl={true}
+            zoomControl={true}
+            doubleClickZoom={true}
+            scrollWheelZoom={true}
+            dragging={true}
+            animate={true}
+            easeLinearity={0.35}
+          >
+            <TileLayer
+              url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            />
+            <Marker position={[context.location.lat, context.location.long]} draggable={true} onDragEnd={setMapCoordinates}>
+              <Popup>
+                Popup for any custom information.
+              </Popup>
+            </Marker>
+          </LeafletMap>
+        </div>
       </div>
     );
 }
