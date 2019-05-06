f108b6113e028735a827919eb879442ca36aad10
Shapour Jahanshahi <shapour@kth.se>
2019-05-06 18:52:47 +0200

    feat: added text to buttons and moved GPS-button

--- src/client/components/BottomNavBar/BottomNavBar.js
+++ src/client/components/BottomNavBar/BottomNavBar.js
@@ -1,7 +1,6 @@
 import React, {useContext, useState} from 'react';
 import FmbContext from '../../context/FmbContext';
 import RegularButton from '../Buttons/RegularButton';
-import GPSIcon from '../Icons/GPSIcon';
 import CartIcon from '../Icons/CartIcon';
 import MapIcon from '../Icons/MapIcon';
 import Cart from '../Cart/Cart';
@@ -11,19 +10,20 @@ import Map from '../Map/Map';
 
 const style = {
   stickToBottom: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'flex-start',
     width: '100%',
     position: 'fixed',
     bottom: 0,
   },
   navBar: {
     backgroundColor: 'rgba(0,0,0,0.8)',
-    display: 'grid',
-    gridTemplateColumns: 'auto auto auto auto',
-    gridColumnGap: themes.standardSpace,
+    display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
     padding: themes.standardSpace,
     boxSizing: 'border-box',
+    height: '46px'
   },
   popup: {
     width: '100%',
@@ -33,29 +33,43 @@ const style = {
   },
   badge: {
     backgroundColor: '#e01a8a',
+    color: '#ffffff',
     borderRadius: '7px',
     fontSize: '1rem',
-    marginLeft: '25px',
-    bottom: '0px',
-    marginBottom: '1.4rem',
+    bottom: '2.5rem',
+    right: '-0.3rem',
     padding: '0.1rem 0.3rem',
-    position: 'absolute',
+    position: 'relative',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  mapContainer: {
+    marginLeft: '1rem'
   }
+}
+
+const ButtonLabel = ({icon, label}) => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      textTransform: 'uppercase',
+      fontSize: '0.6rem'
+    }}>
+      {label}
+      <div style={{marginLeft: '0.4rem'}}>
+        {icon}
+      </div>
+    </div>
+  )
 }
 
 const BottomNavBar = (props) => {
   const context = useContext(FmbContext);
   const [showMap, setShowMap]   = useState(false);
   const [showCart, setShowCart] = useState(false);
-
-  const getCoordinates = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      context.setCoordinates({lat: position.coords.latitude, long: position.coords.longitude});
-    });
-  }
 
   const toggleMap = () => {
     setShowCart(false);
@@ -75,31 +89,26 @@ const BottomNavBar = (props) => {
       </div>
       <div style={style.navBar}>
         <RegularButton
-          label={<GPSIcon color='white' width='24px' />}
-          bgcolor={themes.primaryButton}
-          width='70px'
-          onClick={getCoordinates}
-        />
-        <RegularButton
-          label={<MapIcon color='white' width='24px' />}
+          label={<ButtonLabel label='Ange startplats' icon={<MapIcon color='white' width='20px'/>} />}
           bgcolor={themes.primaryButton}
           color={themes.standardTextColor}
-          width='70px'
           onClick={toggleMap}
         />
-        <RegularButton
-          label={<CartIcon color='white' width='24px' />}
-          bgcolor={themes.primaryButton}
-          color={themes.standardTextColor}
-          width='70px'
-          onClick={toggleCart}
-          badge={
-            context.selectedProducts.length == 0
-            ? null
-            : <div style={style.badge}>
-            {context.selectedProducts.length}
-            </div>}
-        />
+        <div style={style.mapContainer}>
+          <RegularButton
+            label={<ButtonLabel label='Valda produkter' icon={<CartIcon color='white' width='20px' />} />}
+            bgcolor={themes.primaryButton}
+            color={themes.standardTextColor}
+            onClick={toggleCart}
+          />
+          { context.selectedProducts.length > 0 &&
+            <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+              <div style={style.badge}>
+                {context.selectedProducts.length}
+              </div>
+            </div>
+          }
+        </div>
       </div>
     </div>
   );
